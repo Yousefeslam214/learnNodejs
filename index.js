07cf1b65b2efabaff9260ba52598b9e3474c7e10
Yousef eslam <yousefeslam214@gmail.com>
2024-07-06 09:08:22 +0300

    update

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const fs = require('fs')
 
-let hello = 'hello world';
-console.log(hello)
-let textIn = fs.readFileSync('./lorem.txt', 'utf-8')
-console.log(textIn)
-const textOut =`This is what ` + textIn
-fs.writeFileSync('./lorem.txt', textOut)
-console.log('done')
+// Blocking, synchronous way
+// let hello = 'hello world';
+// console.log(hello)
+// let textIn = fs.readFileSync('./lorem.txt', 'utf-8')
+// console.log(textIn)
+// const textOut = `This is what ` + textIn
+// fs.writeFileSync('./lorem.txt', textOut)
+// console.log('done')
+
+// Non-blocking, asynchronous way
+fs.readFile('./lorem.txt', 'utf-8', (err, data1) => {
+    console.log(data1)
+    fs.readFile(`./${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2)
+    })
+})
+console.log('Non-blocking')
